2e1f351717eed6d2d28d9ea3abb375ec75cf7e44
Bianca <mbiancaadr@gmail.com>
2023-12-27 14:16:03 +0200

    files

---
+++ src/constants/moodColors.js
@@ -0,0 +1,16 @@
+export const MoodsBaseColors = {
+    Red: [
+        '#ff2100', '#ff2e00', '#ff4214', '#ff5628', '#ff6a3c'
+    ],
+    Blue: [
+        '#0014ff', '#1428ff', '#283cff', '#3c50ff', '#5064ff'
+    ],
+    Yellow: [
+        '#ffdf14', '#ffe328', '#ffe73c', '#ffeb50', '#ffef64'
+    ],
+    Green: [
+        '#2df042', '#41f255', '#55f468', '#69f67b', '#7df88e'
+    ]
+}
+
+export const DARKEN_COLOR_FACTOR = 0.009;
---
+++ src/constants/moods.js
@@ -0,0 +1,310 @@
+export const Moods = {
+    Red: {
+        'Enraged': {
+            points: 5
+        },
+        'Panicked': {
+            points: 4
+        },
+        'Stressed': {
+            points: 10
+        },
+        'Jittery': {
+            points: 10
+        },
+        'Shocked': {
+            points: 10
+        },
+        'Livid': {
+            points: 10
+        },
+        'Furious': {
+            points: 10
+        },
+        'Frustrated': {
+            points: 10
+        },
+        'Tense': {
+            points: 10
+        },
+        'Stunned': {
+            points: 10
+        },
+        'Fuming': {
+            points: 10
+        },
+        'Frightened': {
+            points: 10
+        },
+        'Angry': {
+            points: 10
+        },
+        'Nervous': {
+            points: 10
+        },
+        'Restless': {
+            points: 10
+        },
+        'Anxious': {
+            points: 10
+        },
+        'Apprehensive': {
+            points: 10
+        },
+        'Worried': {
+            points: 10
+        },
+        'Irritated': {
+            points: 10
+        },
+        'Annoyed': {
+            points: 10
+        },
+        'Repulsed': {
+            points: 10
+        },
+        'Troubled': {
+            points: 10
+        },
+        'Concerned': {
+            points: 10
+        },
+        'Uneasy': {
+            points: 10
+        },
+        'Peeved': {
+            points: 10
+        }
+    },
+    Yellow: {
+        'Surprised': {
+            points: 10
+        },
+        'Upbeat': {
+            points: 10
+        },
+        'Festive': {
+            points: 10
+        },
+        'Exhilarated': {
+            points: 10
+        },
+        'Ecstatic': {
+            points: 10
+        },
+        'Hyper': {
+            points: 10
+        },
+        'Cheerful': {
+            points: 10
+        },
+        'Motivated': {
+            points: 10
+        },
+        'Inspired': {
+            points: 10
+        },
+        'Elated': {
+            points: 10
+        },
+        'Energised': {
+            points: 10
+        },
+        'Lively': {
+            points: 10
+        },
+        'Enthusiastic': {
+            points: 10
+        },
+        'Optimistic': {
+            points: 10
+        },
+        'Excited': {
+            points: 10
+        },
+        'Pleased': {
+            points: 10
+        },
+        'Happy': {
+            points: 10
+        },
+        'Focused': {
+            points: 10
+        },
+        'Proud': {
+            points: 10
+        },
+        'Thrilled': {
+            points: 10
+        },
+        'Pleasant': {
+            points: 10
+        },
+        'Joyful': {
+            points: 10
+        },
+        'Hopeful': {
+            points: 10
+        },
+        'Playful': {
+            points: 10
+        },
+        'Blissful': {
+            points: 10
+        }
+    },
+    Blue: {
+        'Disgusted': {
+            points: 10
+        },
+        'Glum': {
+            points: 10
+        },
+        'Disappointed': {
+            points: 10
+        },
+        'Down': {
+            points: 10
+        },
+        'Apathetic': {
+            points: 10
+        },
+        'Pessimistic': {
+            points: 10
+        },
+        'Morose': {
+            points: 10
+        },
+        'Discouraged': {
+            points: 10
+        },
+        'Sad': {
+            points: 10
+        },
+        'Bored': {
+            points: 10
+        },
+        'Alienated': {
+            points: 10
+        },
+        'Miserable': {
+            points: 10
+        },
+        'Lonely': {
+            points: 10
+        },
+        'Disheartened': {
+            points: 10
+        },
+        'Tired': {
+            points: 10
+        },
+        'Despondent': {
+            points: 10
+        },
+        'Depressed': {
+            points: 10
+        },
+        'Sullen': {
+            points: 10
+        },
+        'Exhausted': {
+            points: 10
+        },
+        'Fatigued': {
+            points: 10
+        },
+        'Despair': {
+            points: 10
+        },
+        'Hopeless': {
+            points: 10
+        },
+        'Desolated': {
+            points: 10
+        },
+        'Spent': {
+            points: 10
+        },
+        'Drained': {
+            points: 10
+        }
+    },
+    Green: {
+        'At ease': {
+            points: 10
+        },
+        'Easygoing': {
+            points: 10
+        },
+        'Content': {
+            points: 10
+        },
+        'Loving': {
+            points: 10
+        },
+        'Fulfilled': {
+            points: 10
+        },
+        'Calm': {
+            points: 10
+        },
+        'Secure': {
+            points: 10
+        },
+        'Satisfied': {
+            points: 10
+        },
+        'Grateful': {
+            points: 10
+        },
+        'Touched': {
+            points: 10
+        },
+        'Relaxed': {
+            points: 10
+        },
+        'Chill': {
+            points: 10
+        },
+        'Restful': {
+            points: 10
+        },
+        'Blessed': {
+            points: 10
+        },
+        'Balanced': {
+            points: 10
+        },
+        'Mellow': {
+            points: 10
+        },
+        'Thoughtful': {
+            points: 10
+        },
+        'Peaceful': {
+            points: 10
+        },
+        'Comfy': {
+            points: 10
+        },
+        'Carefree': {
+            points: 10
+        },
+        'Sleepy': {
+            points: 10
+        },
+        'Complacent': {
+            points: 10
+        },
+        'Tranquil': {
+            points: 10
+        },
+        'Cozy': {
+            points: 10
+        },
+        'Serene': {
+            points: 10
+        }
+    }
+}
